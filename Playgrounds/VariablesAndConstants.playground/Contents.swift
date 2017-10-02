3eac8f535ef8e0cec87e458fa697dde829b89b1d
Kyrill van Seventer <k.vanseventer@gmail.com>
2017-10-02 12:54:39 +0200

    initial commit

---
+++ Playgrounds/VariablesAndConstants.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+var schoolName: String
+var name = "Kyrill van Seventer"
+var age = 22
+var agenew = 22.0
+
+schoolName = "The App Academy"
+
+//print("My name is \(name) and I am \(age) years old.")
+
+var daysInTheWeek = 7
+var hoursInTheDay = 24
+var minutesInAnHour = 60
+//var minutesInAnWeek = minutesInAnHour * hoursInTheDay * daysInTheWeek
+
+//print(minutesInAnHour * hoursInTheDay * daysInTheWeek)
+
+var modulus = 10 % 3
+//print(modulus)
+
+//print(24/3)
+//print(24-3)
+//print(24+3)
+
+var finalScore = 0
+print (finalScore)
+
+while finalScore != 100 {
+    finalScore += 10
+    print(finalScore)
+}
+
+var test: Double = 1.0001
+var test1 = 2
+
+print(test + Double(test1))
+
