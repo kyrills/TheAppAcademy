2903e69754dd34c3724e6f268f64fe6444513587
Kyrill van Seventer <k.vanseventer@gmail.com>
2017-10-10 13:09:12 +0200

    fixed

---
+++ TabbedKata/TabbedKata/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedKata
+//
+//  Created by Kyrill van Seventer on 10/10/2017.
+//  Copyright © 2017 Kyrill van Seventer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
