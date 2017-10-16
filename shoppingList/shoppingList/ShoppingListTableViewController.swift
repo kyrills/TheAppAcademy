c1303ad09e88316cf8ebe8c5ca29b68096f360ce
Kyrill van Seventer <k.vanseventer@gmail.com>
2017-10-16 08:27:02 +0200

    Added new application and pushed changes to older apps

--- shoppingList/shoppingList/ShoppingListTableViewController.swift
+++ shoppingList/shoppingList/ShoppingListTableViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class ShoppingListTableViewController: UITableViewController {
-
+    
+    var productDictionary = ["Red Bull" : #imageLiteral(resourceName: "RedBull"), "Bread" : #imageLiteral(resourceName: "Bread"), "Pasta" : #imageLiteral(resourceName: "Pasta"), "Cola" : #imageLiteral(resourceName: "Cola")]
+    var dictKeys: [String]?
     override func viewDidLoad() {
         super.viewDidLoad()
+        let keys = productDictionary.keys
+        dictKeys = Array(keys)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,24 +28,28 @@ class ShoppingListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return productDictionary.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celliD", for: indexPath)
+        cell.imageView?.image = productDictionary[dictKeys![indexPath.row]]
+        cell.textLabel?.text = dictKeys![indexPath.row]
+
 
         // Configure the cell...
 
         return cell
     }
-    */
-
+    
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
